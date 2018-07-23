f4c061147951fdb115c48a9b1768e77351c3ffe8
AzureAD\GwonYoungKwon <gtah2yk@gmail.com>
2018-07-24 00:37:17 +0900

    add : navigation

---
+++ src/components/Navigation/Navigation.vue
@@ -0,0 +1,28 @@
+<template>
+<nav class="Navigation">
+  <div class="wrap">
+    <div class="container">
+
+    </div>
+  </div>
+</nav>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang='scss' scoped>
+.Navigation {
+  .wrap {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    position: relative;
+      .container{
+        height:90%;
+        background-color: #000000;
+      }
+  }
+}
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="home">
+  <navigation></navigation>
+</div>
+</template>
+
+<script>
+import Navigation from '../components/Navigation/Navigation'
+export default {
+  name: 'home',
+  components: {
+    Navigation
+  }
+}
+</script>
+<style lang='scss' scoped>
+
+</style>
